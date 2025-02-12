d55d14ad8be51781021313cfb28e4452ba4ae98b
Manawyrm <git@tbspace.de>
2025-02-12 09:35:08 +0000

    add assets folder

---
+++ assets/prelistening.js
@@ -0,0 +1,55 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+<!--
+
+var prelWin = "";
+
+function MM_openBrWindow(theURL,winName,features)
+
+{
+
+  if(prelWin.closed == false)
+
+  {
+
+    prelWin.close();
+
+  }
+
+  prelWin = window.open(theURL,winName,features);
+
+}
+
+//-->
+
+}
+/*
+     FILE ARCHIVED ON 14:52:31 Apr 03, 2005 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 23:36:45 May 06, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 119.925
+  exclusion.robots: 0.082
+  exclusion.robots.policy: 0.071
+  RedisCDXSource: 0.559
+  esindex: 0.009
+  LoadShardBlock: 99.133 (3)
+  PetaboxLoader3.datanode: 195.06 (4)
+  load_resource: 203.222
+  PetaboxLoader3.resolve: 80.19
+*/
